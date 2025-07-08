f8b5354b39014f7edd9d5affe3623e58b8be970a
Paul Sebastian <pnaspudv@est.ups.edu.ec>
2025-07-07 22:02:23 -0500

    AutoScaling yml

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -1,4 +1,4 @@
-import { Libro } from '../entity/libro';
+import { Libro } from "../entity/libro";
 import { DataSource } from "typeorm";
 import "reflect-metadata";
 import dotenv from "dotenv";
@@ -8,11 +8,10 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,       
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
   entities: [Libro],
 });
-
